6c676b191dbe156e573693477029c0aefc3b1a87
dzuk-mutant <32108484+dzuk-mutant@users.noreply.github.com>
2019-02-18 09:01:56 +0000

    Removed special case for U+0 in glyf table generator

--- tables/glyf.py
+++ tables/glyf.py
@@ -10,31 +10,6 @@ def glyf(glyphs):
 
 
     for ID, g in enumerate(glyphs):
-
-        if ID is 0:
-            # make a dummy contour for .notdef.
-            # for some reason (according to macOS validation) it's important.
-            # the others can be blank
-
-            # These attributes will be calculated by the compiler.
-            dummyDataNotDef = Element("TTGlyph",    {"name": g.name
-                                                    ,"xMin": "0"
-                                                    ,"xMax": "0"
-                                                    ,"yMin": "0"
-                                                    ,"yMax": "0"
-                                                    })
-            dummyContours = Element("contour")
-            dummyContours.append(Element("pt", {"x": "100", "y": "100", "on": "1"}))
-
-            dummyDataNotDef.append(dummyContours)
-            dummyDataNotDef.append(Element("instructions"))
-
-            glyfTable.append(dummyDataNotDef)
-
-        else:
-            # make the others blank because nothing is depending on
-            # them actually having glyf contours and we don't want them.
-
             glyfTable.append(Element("TTGlyph", {"name": g.name}))
 
 
